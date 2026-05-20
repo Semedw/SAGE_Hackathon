f8f67f4d161b61d30e8a5c14c448780670320d2d
Semedw <smusazade2000@gmail.com>
2026-05-20 18:38:44 +0400

    some changes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -108,6 +108,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
